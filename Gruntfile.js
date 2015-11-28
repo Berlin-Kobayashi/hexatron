a8bf06331b76c67e313cfa87f3a529ffc059f07b
DanShu <d.schulz93@gmx.de>
2015-11-28 13:55:04 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		babel: {
+			options: {
+				presets: ['es2015']
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['js/**/*.es6'],
+					dest: 'compiled/',
+					ext: ".js"
+				}]
+			}
+		},
+		bower: {
+			install: {
+				options: {
+					copy: false
+				}
+			}
+		},
+		browserify: {
+			'dist/js/app.js': ['compiled/js/app.js']
+		}
+	});
+
+	grunt.registerTask('default', ['bower:install', 'babel', 'browserify']);
+	grunt.registerTask('build-js', ['babel', 'browserify']);
+};
